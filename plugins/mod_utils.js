eab47c7f624625cf92bd83dc1b78a95604c4af77
JTK222 <lukasporwit3@googlemail.com>
2019-09-19 10:04:17 +0200

    Re-Implemented Techne support for Mod-Utils plugin

--- plugins/mod_utils.js
+++ plugins/mod_utils.js
@@ -30,11 +30,21 @@ function loadZipToJson(importType){
 							importType.import(json);
 						});
 						
-						var imgFile = zip.file(importType.texture).async("base64").then(img => {
-							var texture = new Texture().fromDataURL('data:image/png;base64,' + img);
-						
-							texture.add();
-						});
+						if(importType == ImportTypeEnum.TBL){
+							var imgFile = zip.file(importType.texture).async("base64").then(img => {
+								var texture = new Texture().fromDataURL('data:image/png;base64,' + img);
+							
+								texture.add();
+							});
+						}else{
+							var imgFile = zip.file(importType.texture).forEach(pr => {
+								pr.async("base64").then(img => {
+									var texture = new Texture().fromDataURL('data:image/png;base64,' + img);
+								
+									texture.add();
+								});
+							});
+						}
 					});
 			});
 		}
@@ -50,8 +60,8 @@ var ImportTypeEnum = {
 	},
 	TCN: {
 		extension: 'tcn',
-		file: 'model.json',
-		texture: 'texture.png',
+		file: 'model.xml',
+		texture: /\.png/,
 		import: loadTechneModel
 	}
 }
@@ -89,7 +99,7 @@ var helpDialog = new Dialog({
 	title: 'Help - Mod Utils',
 	width: 800,
 	lines: [
-		'<style> .modUtilHelpTabs { position: relative; min-height: 200px; /* This part sucks */ clear: both; margin: 25px 0; } .modUtilsHelpTab { float: left; } .modUtilsHelpTab label { background: var(--color-ui); padding: 10px; border: 1px solid #ccc; margin-left: -1px; position: relative; left: 1px; } .modUtilsHelpTab [type=radio] { display: none; } .modUtilsContent { position: absolute; top: 28px; left: 0; background: var(--color-ui); right: 0; bottom: 0; padding: 20px; border: 1px solid #ccc; height: auto; overflow: auto; } [type=radio]:checked ~ label { background: var(--color-button); border-bottom: 1px solid white; z-index: 2; } [type=radio]:checked ~ label ~ .modUtilsContent { z-index: 1; } </style> <div class="modUtilHelpTabs"> <div class="modUtilsHelpTab"><input type="radio" id="tab-1" name="tab-group-1" checked> <label for="tab-1">VoxelShapes</label> <div class="modUtilsContent"> <p> In order to use the VoxelShape exporter, you first need to create a new Group, called "VoxelShapes". All cubes that you create within this group, will be added to the voxelShape trough the OR BooleanFunction. Additionally you can add sub groups with the name equaling the BooleanFunctions shown in the image bellow. The first cube in such a group does represent the red cube, all other ones will be combined with an OR BooleanFunction first. </p> <img src="https://dark-roleplay.net/files/VoxelShapeGuideCropped.png" width="100%" height="auto"/> </div> </div> <div class="modUtilsHelpTab"><input type="radio" id="tab-2" name="tab-group-1"><label for="tab-2">Tabula Import</label> <div class="modUtilsContent"> <p>In order to import a Tabula Model, you need to create a new Modded Entity. Now the Point "Import Tabula Model (.tbl)" should be available in your import menu.</p> </div> </div> <div class="modUtilsHelpTab"><input type="radio" id="tab-3" name="tab-group-1"><label for="tab-3">Techne Import</label> <div class="modUtilsContent"> <p>The Techne model file import is currently disabled. If you have .tcn model file, please contact "JTK222 | Lukas#7285" on discord, so we can re-add the support</p> </div> </div> </div>'
+		'<style> .modUtilHelpTabs { position: relative; min-height: 200px; /* This part sucks */ clear: both; margin: 25px 0; } .modUtilsHelpTab { float: left; } .modUtilsHelpTab label { background: var(--color-ui); padding: 10px; border: 1px solid #ccc; margin-left: -1px; position: relative; left: 1px; } .modUtilsHelpTab [type=radio] { display: none; } .modUtilsContent { position: absolute; top: 28px; left: 0; background: var(--color-ui); right: 0; bottom: 0; padding: 20px; border: 1px solid #ccc; height: auto; overflow: auto; } [type=radio]:checked ~ label { background: var(--color-button); border-bottom: 1px solid white; z-index: 2; } [type=radio]:checked ~ label ~ .modUtilsContent { z-index: 1; } </style> <div class="modUtilHelpTabs"> <div class="modUtilsHelpTab"> <input type="radio" id="tab-1" name="tab-group-1" checked> <label for="tab-1">VoxelShapes</label> <div class="modUtilsContent"> <p> In order to use the VoxelShape exporter, you first need to create a new Group, called "VoxelShapes". All cubes that you create within this group, will be added to the voxelShape trough the OR BooleanFunction. Additionally you can add sub groups with the name equaling the BooleanFunctions shown in the image bellow. The first cube in such a group does represent the red cube, all other ones will be combined with an OR BooleanFunction first. </p> <img src="https://dark-roleplay.net/files/VoxelShapeGuideCropped.png" width="100%" height="auto" /> </div> </div> <div class="modUtilsHelpTab"> <input type="radio" id="tab-2" name="tab-group-1"> <label for="tab-2">Tabula Import</label> <div class="modUtilsContent"> <p>In order to import a Tabula Model, you need to create a new Modded Entity. Now the Point "Import Tabula Model (.tbl)" should be available in your import menu.</p> </div> </div> <div class="modUtilsHelpTab"> <input type="radio" id="tab-3" name="tab-group-1"> <label for="tab-3">Techne Import</label> <div class="modUtilsContent"> <p>Techne Import is only Available in Modded Entity Mode. A new Menu entry under "File > Import > Import Techne Model (.tcn)" should be available.</p> </div> </div> </div>'
 	],
 	singleButton: true
 });
@@ -379,7 +389,7 @@ Plugin.register('mod_utils', {
 			
 			MenuBar.addAction(exportVoxelShapeAction, 'file.export');
 			MenuBar.addAction(importTabula, 'file.import');
-			//MenuBar.addAction(importTechne, 'file.import');
+			MenuBar.addAction(importTechne, 'file.import');
 			MenuBar.addAction(modUtilsHelp, 'help');
 		}
 	},
@@ -387,7 +397,7 @@ Plugin.register('mod_utils', {
 		if(isValidVersion){
 			exportVoxelShapeAction.delete();
 			importTabula.delete();
-			//importTechne.delete();
+			importTechne.delete();
 			modUtilsHelp.delete();
 
 			helpMenu.hide();
