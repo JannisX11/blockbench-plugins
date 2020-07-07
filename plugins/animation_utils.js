778a702456926afff2d82bddf1e80eb285ca81e3
Eliot Lash <fadookie@gmail.com>
2020-07-07 07:18:21 -0700

    remove vestigial code and comments

--- plugins/animation_utils.js
+++ plugins/animation_utils.js
@@ -10,49 +10,6 @@
 		return Math.floor(num);
 	}
 
-	// For monkeypatching constructors from https://github.com/jamesallardice/patchwork.js
-	const patch = (function () {
-    /*jshint evil: true */
-
-    "use strict";
-
-    var global = new Function("return this;")(), // Get a reference to the global object
-        fnProps = Object.getOwnPropertyNames(Function); // Get the own ("static") properties of the Function constructor
-
-    return function (original, originalRef, patches) {
-
-        var ref = global[originalRef] = original, // Maintain a reference to the original constructor as a new property on the global object
-            args = [],
-            newRef, // This will be the new patched constructor
-            i;
-
-        patches.called = patches.called || originalRef; // If we are not patching static calls just pass them through to the original function
-
-        for (i = 0; i < original.length; i++) { // Match the arity of the original constructor
-            args[i] = "a" + i; // Give the arguments a name (native constructors don't care, but user-defined ones will break otherwise)
-        }
-
-        if (patches.constructed) { // This string is evaluated to create the patched constructor body in the case that we are patching newed calls
-            args.push("'use strict'; return (!!this ? " + patches.constructed + " : " + patches.called + ").apply(null, arguments);"); 
-        } else { // This string is evaluated to create the patched constructor body in the case that we are only patching static calls
-            args.push("'use strict'; return (!!this ? new (Function.prototype.bind.apply(" + originalRef + ", [{}].concat([].slice.call(arguments))))() : " + patches.called + ".apply(null, arguments));");
-        }
-
-        newRef = new (Function.prototype.bind.apply(Function, [{}].concat(args)))(); // Create a new function to wrap the patched constructor
-        newRef.prototype = original.prototype; // Keep a reference to the original prototype to ensure instances of the patch appear as instances of the original
-        newRef.prototype.constructor = newRef; // Ensure the constructor of patched instances is the patched constructor
-
-        Object.getOwnPropertyNames(ref).forEach(function (property) { // Add any "static" properties of the original constructor to the patched one
-            if (fnProps.indexOf(property) < 0) { // Don't include static properties of Function since the patched constructor will already have them
-                newRef[property] = ref[property];
-            }
-        });
-
-        return newRef; // Return the patched constructor
-    };
-
-}());
-
 function lerp(start, stop, amt) {
   return amt * (stop - start) + start;
 };
@@ -325,10 +282,6 @@ import software.bernie.geckolib.forgetofabric.ResourceLocation;`;
 					// 	return n;
 					// }
 					const keyframe = document.getElementById('keyframe');
-					// console.log(`updateKeyframeSelection:`, args, ' keyframe:', keyframe);
-					// const easingBar = $(`<div class="bar flex" id="keyframe_bar_easing">
-					// 	<label class="tl" style="font-weight: bolder; min-width: 47px;">Easing</label>
-					// </div>`);
 					let easingBar = document.createElement('div');
 					keyframe.appendChild(easingBar);
 					easingBar.outerHTML = `<div class="bar flex" id="keyframe_bar_easing">
@@ -336,9 +289,6 @@ import software.bernie.geckolib.forgetofabric.ResourceLocation;`;
 					</div>`;
 					easingBar = document.getElementById('keyframe_bar_easing');
 
-					// var el = $(`<div class="bar_select half"><select class="focusable_input" id="${form_id}"></select></div>`)
-					// const el = $(`<select class="focusable_input" id="keyframe_easing" style="flex: 1; margin-right: 9px;"></select>`)
-					// const sel = el.find('select')
 					let sel = document.createElement('select');
 					easingBar.appendChild(sel);
 					sel.outerHTML = `<select class="focusable_input" id="keyframe_easing" style="flex: 1; margin-right: 9px;" oninput="updateKeyframeEasing(this)"></select>`;
@@ -347,26 +297,14 @@ import software.bernie.geckolib.forgetofabric.ResourceLocation;`;
 						var name = EASING_OPTIONS[key];
 						const option = document.createElement('option')
 						sel.appendChild(option);
-						// option.outerHTML = `<option id="${key}" ${first.easing || EASING_DEFAULT === key ? 'selected' : ''}>${name}</option>`;
 						option.outerHTML = `<option id="${key}" ${(first.easing || EASING_DEFAULT) === key ? 'selected' : ''}>${name}</option>`;
 					}
-					// easingBar.append(el)
-						// <input type="text" id="keyframe_easing" axis="easing" class="dark_bordered code keyframe_input tab_target" style="flex: 1; margin-right: 9px;" oninput="updateKeyframeEasing(this)"></input>
 					console.log('easingBar:', easingBar, 'keyframe:', keyframe);
-					// $('#keyframe_bar_easing input').val(first.easing || '');
 			}
 		}
 	};
 
 	const KeyframeGetLerpOriginal = Keyframe.prototype.getLerp;
-	// getLerp(other, axis, amount, allow_expression) {
-	// 	if (allow_expression && this.get(axis) === other.get(axis)) {
-	// 		return this.get(axis)
-	// 	} else {
-	// 		let calc = this.calc(axis)
-	// 		return calc + (other.calc(axis) - calc) * amount
-	// 	}
-	// }
 	function keyframeGetLerp(other, axis, amount, allow_expression) {
 			const easing = this.easing || EASING_DEFAULT;
 			if (Format.id !== "animated_entity_model") {
@@ -417,14 +355,6 @@ import software.bernie.geckolib.forgetofabric.ResourceLocation;`;
 		return result;
 	}
 
-	function keyframeConstructor(data, uuid) {
-			// const result = new (Function.prototype.bind.apply(KeyframeOriginal, arguments));//[{}].concat(arguments)));
-			const result = new (Function.prototype.bind.apply(KeyframeOriginal, [{}, ...arguments]));
-			// if (Format.id === "animated_entity_model") Object.assign(result, { easing: data.easing });
-			// console.log('keyframeConstructor arguments:', arguments, 'this:', this, 'result:', result);
-			return result;
-	}
-
 	Plugin.register("animation_utils", {
 		name: "Gecko's Animation Utils",
 		author: "Gecko",
@@ -443,9 +373,6 @@ import software.bernie.geckolib.forgetofabric.ResourceLocation;`;
 			Keyframe.prototype.getArray = keyframeGetArray;
 			Keyframe.prototype.getUndoCopy = keyframeGetUndoCopy;
 			Keyframe.prototype.extend = keyframeExtend;
-			// Keyframe = patch(Keyframe, "KeyframeOriginal", {
-			// 	constructed: keyframeConstructor,
-			// });
 			
 			global.updateKeyframeEasing = updateKeyframeEasing;
 			
@@ -499,7 +426,6 @@ import software.bernie.geckolib.forgetofabric.ResourceLocation;`;
 			exportAction.delete();
 			button.delete();
 			delete global.updateKeyframeEasing;
-	 		// Keyframe = KeyframeOriginal;
 			Keyframe.prototype.getLerp = KeyframeGetLerpOriginal;
 			Keyframe.prototype.getArray = KeyframeGetArrayOriginal;
 			Keyframe.prototype.getUndoCopy = KeyframeGetUndoCopyOriginal;
