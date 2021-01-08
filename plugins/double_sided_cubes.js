bd4c9fb71c656c2a183e5487a5fa664ab62877c3
SnaveSutit <snavesutit@gmail.com>
2021-01-08 15:14:19 -0500

    Update double_sided_cubes.js

--- plugins/double_sided_cubes.js
+++ plugins/double_sided_cubes.js
@@ -1,36 +1,6 @@
 (()=> {
-	const cube_action = new Action({
-		id:"create_double_sided_cubes",
-		name: 'Create Double Sided Cube',
-		icon: 'flip_to_back',
-		category: 'edit',
-		condition: () => !Project.box_uv,
-		click: function(ev) {
-			if (selected.length === 0) {
-				Blockbench.showMessage('No cubes selected', 'center')
-				return;
-			};
-			Undo.initEdit({elements:[]});
-			let cubes = [];
-			Cube.selected.forEach((cube) => {
-				const new_cube = new Cube({...cube}).init();
-				new_cube.name = new_cube.name + " inverted";
-				if (cube.parent !== "root") {
-					new_cube.addTo(cube.parent);
-				};
-				for (i=0; i<3; i++){
-					new_cube.flip(i, 0, false)
-				};
-				new_cube.from = [...cube.to];
-				new_cube.to = [...cube.from];
-				new_cube.inflate = -new_cube.inflate;
-				Canvas.adaptObjectPosition(new_cube);
-				Canvas.updateUV(new_cube);
-				cubes.push(new_cube);
-			});
-			Undo.finishEdit('create_double_sided_cube', {elements:cubes});
-		}
-	});
+
+	let cube_action;
 
 	Plugin.register('double_sided_cubes', {
 		title: 'Double Sided Cubes',
@@ -40,6 +10,38 @@
 		version: '1.0.0',
 		variant: 'both',
 		onload() {
+			cube_action = new Action({
+				id:"create_double_sided_cubes",
+				name: 'Create Double Sided Cube',
+				icon: 'flip_to_back',
+				category: 'edit',
+				condition: () => !Project.box_uv,
+				click: function(ev) {
+					if (selected.length === 0) {
+						Blockbench.showMessage('No cubes selected', 'center')
+						return;
+					};
+					Undo.initEdit({elements:[]});
+					let cubes = [];
+					Cube.selected.forEach((cube) => {
+						const new_cube = new Cube({...cube}).init();
+						new_cube.name = new_cube.name + " inverted";
+						if (cube.parent !== "root") {
+							new_cube.addTo(cube.parent);
+						};
+						for (i=0; i<3; i++){
+							new_cube.flip(i, 0, false)
+						};
+						new_cube.from = [...cube.to];
+						new_cube.to = [...cube.from];
+						new_cube.inflate = -new_cube.inflate;
+						Canvas.adaptObjectPosition(new_cube);
+						Canvas.updateUV(new_cube);
+						cubes.push(new_cube);
+					});
+					Undo.finishEdit('create_double_sided_cube', {elements:cubes});
+				}
+			});
 			MenuBar.addAction(cube_action, 'filter');
 		},
 		onunload() {
