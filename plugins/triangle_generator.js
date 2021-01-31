fe904a91bf8dcc273879ce9402e14f0935237ff4
David Morales <sleepyhead080808@gmail.com>
2021-01-31 17:49:53 -0500

    Updated to new BlockBench framework

--- plugins/triangle_generator.js
+++ plugins/triangle_generator.js
@@ -1,97 +1,68 @@
-var plugin_data = {
-    id: 'triangle_generator',
-    title: 'Triangle Generator',
-    icon: 'pages',
-    author: 'Sleepyhead08',
-    description: 'Generates triangles.',
-    version: '1.0.0',
-    min_version: '3.0.2',
-    variant: 'both'
-};
 
 (function() {
 
-var triangle_shape_list = `
-        <select style="color:var(--color-text), class=" id="triangle_shape">
-        <option selected=true value="i_triangle"> Isoceles Triangle </option>
-        <option value="r_triangle"> Right Triangle </option>
-        <option value="pyramid"> Pyramid </option>
-    </select>`;
+var generate_triangle_action;
 
-Blockbench.addMenuEntry('Generate triangle','pages', function(){
+Plugin.register('triangle_generator', 
+	{
+		title: 'Triangle Generator',
+	    icon: 'pages',
+	    author: 'Sleepyhead08',
+	    description: 'Generates triangles.',
+	    version: '1.0.0',
+	    variant: 'both',
 
-    // Check if the user is in the right mode.
-    if (!Format.rotate_cubes) {
-        Blockbench.showMessageBox({
-            title: 'Incompatible Format',
-            message: 'This plugin only works in formats that support cube rotations.'
-        })
-        return;
-    }
-    else {
-        triangleWindow();
-    }
-});
+		onload() {
+			generate_triangle_action = new Action({
+				id: 'triangle_generator_action',
+				name: 'Generate Triangle',
+				icon: 'pages',
+				category: 'filter',
+				click: function(ev) {
+					getTriangleWindow().show();
+				}
+			});
 
-// Triangle selection window.
-function triangleWindow() {
+			MenuBar.addAction(generate_triangle_action, 'filter');
+		},
+		onunload() {
+			this.onuninstall();
+		},
+		onuninstall() {
+			generate_triangle_action.delete();
+		}
+	})
+
+
+function getTriangleWindow(){
+
     var triangle_window = new Dialog({
-        title: 'Triangle Generator', id: 'triangle_selector', lines: [
-            'Select triangle:' + triangle_shape_list +
-            '<br\>Origin: <br\><input value=8 type="number" style="background-color:var(--color-back)" id="origin_x"> <br\><input value=0 type="number" style="background-color:var(--color-back)" id="origin_y"> <br\><input value=8 type="number" style="background-color:var(--color-back)" id="origin_z"> ' + 
-            '<br\>Height: <input value=1 type="number" style="background-color:var(--color-back)" id="height">' + 
-            '<br\>Width: <input value=0.1 type="number" style="background-color:var(--color-back)" id="width">' +
-            '<br\>Angle: <input value=45 type="number" style="background-color:var(--color-back)" id="angle">'  +
-            '<br\>Granularity: <input value=0.1 type="number" style="background-color:var(--color-back)" id="granularity"><br\>' 
-        ], draggable: true, onConfirm() {
-            triangle_window.hide();
-            generate_triangle();
-        }
-    });
-    triangle_window.show();
-    if (localStorage.getItem("triangle_shape") != undefined) {
-        document.getElementById("triangle_shape").selectedIndex = localStorage.getItem("triangle_shape");
-        $('.dialog#triangle_selector input#origin_x').val(localStorage.getItem("origin_x"));
-        $('.dialog#triangle_selector input#origin_y').val(localStorage.getItem("origin_y"));
-        $('.dialog#triangle_selector input#origin_z').val(localStorage.getItem("origin_z"));
-        $('.dialog#triangle_selector input#height').val(localStorage.getItem("height"));
-        $('.dialog#triangle_selector input#width').val(localStorage.getItem("width"));
-        $('.dialog#triangle_selector input#angle').val(localStorage.getItem("angle"));
-        $('.dialog#triangle_selector input#granularity').val(localStorage.getItem("granularity"));
-    }
+        id: 'triangle_selector', 
+        title: 'Triangle Generator', 
+        form: {
+        	triangle_shape: {label: 'Select triangle', type: 'select', options: {i_triangle: 'Isoceles Triangle', r_triangle: 'Right Triangle', pyramid: 'Pyramid'}, default: 'i_triangle'},
+        	origin_x: {label: 'Origin X', type: 'number', value: 8},
+        	origin_y: {label: 'Origin Y', type: 'number', value: 0},
+        	origin_z: {label: 'Origin Z', type: 'number', value: 8},
+        	height: {label: 'Height', type: 'number', value: 1},
+        	width: {label: 'Width', type: 'number', value: 0.1},
+        	angle: {label: 'Angle', type: 'number', value: 45, min: 0, max: 90},
+        	granularity: {label: 'Granularity', type: 'number', value: 0.1}
+        },
+		onConfirm: function(formData) {
+			//generate_triangle(formData);
+			determine_shape(formData.triangle_shape, formData.origin_x, formData.origin_y, formData.origin_z, formData.height, formData.width, formData.angle, formData.granularity);
+			this.hide();
+		},
+		onCancel: function(formData) {
+			this.hide();
+		}
+	});
+	
+	return triangle_window;
 }
 
-function generate_triangle() {
 
-    // Get all of the input.
-    var triangle_shape = $('.dialog#triangle_selector #triangle_shape')[0].value;
-    var triangle_shape_selected = document.getElementById("triangle_shape").selectedIndex;
-    localStorage.setItem("triangle_shape", triangle_shape_selected);
-
-    var origin_x = $('.dialog#triangle_selector input#origin_x').val().valueOf();
-    localStorage.setItem("origin_x", origin_x);
-
-    var origin_y = $('.dialog#triangle_selector input#origin_y').val().valueOf();
-    localStorage.setItem("origin_y", origin_y);
-
-    var origin_z = $('.dialog#triangle_selector input#origin_z').val().valueOf();
-    localStorage.setItem("origin_z", origin_z);
-
-    var height = $('.dialog#triangle_selector input#height').val().valueOf();
-    localStorage.setItem("height", height);
-
-    var width = $('.dialog#triangle_selector input#width').val().valueOf();
-    localStorage.setItem("width", width);
-
-    var angle = $('.dialog#triangle_selector input#angle').val().valueOf();
-    localStorage.setItem("angle", angle);
-
-    var granularity = $('.dialog#triangle_selector input#granularity').val().valueOf();
-    localStorage.setItem("granularity", granularity);
-
-    // Determine which triangle to generate and generate it.
-    determine_shape(triangle_shape, origin_x, origin_y, origin_z, height, width, angle, granularity);
-}
 
 // Determines which triangle to generate and generates it.
 function determine_shape(triangle_shape, origin_x, origin_y, origin_z, height, width, angle, granularity) {
@@ -109,15 +80,59 @@ function determine_shape(triangle_shape, origin_x, origin_y, origin_z, height, w
 
     // Make sure the origin is valid.
     if ((origin_x > 32 || origin_x < -16 || origin_y > 32 || origin_y < -16 || origin_z > 32 || origin_z < -16) && Format.canvas_limit) {
-        var error_window6 = new Dialog({title: 'Error', id: 'error_window_6', lines: [
-            'Error occurred:<br\>' +
-            'Your origin is outside of the restricted area.'
-        ], draggable: true, onConfirm() {
-            error_window6.hide();
-            triangleWindow();
-        }
-        });
-        error_window6.show();
+        var error_window = new Dialog(
+        	{
+            	id: 'error_window', 
+            	title: 'Error', 
+            	lines: [
+	                'Error occurred:\n',
+	                'Your triangle would go outside of the restricted area.\n',
+		            'Please make sure to keep the triangle within the canvas.'
+	            ],
+	            onConfirm() {
+	                error_window.hide();
+	            }
+        	});
+        error_window.cancelEnabled = false;
+        error_window.show();
+        return;
+    }
+
+    //Make sure the granularity is valid.
+    if (granularity <= 0) {
+        var error_window = new Dialog(
+        	{
+            	id: 'error_window', 
+            	title: 'Error', 
+            	lines: [
+	                'Error occurred:\n',
+	                'Granularity must be greater than zero.\n'
+	            ],
+	            onConfirm() {
+	                error_window.hide();
+	            }
+        	});
+        error_window.cancelEnabled = false;
+        error_window.show();
+        return;
+    }
+
+    //Make sure the angle is between 0 and 90 degrees.
+    if (angle <= 0 || angle >= 90) {
+        var error_window = new Dialog(
+        	{
+            	id: 'error_window', 
+            	title: 'Error', 
+            	lines: [
+	                'Error occurred:\n',
+	                'Angle must be between 0 and 90 degrees (exclusive).\n'
+	            ],
+	            onConfirm() {
+	                error_window.hide();
+	            }
+        	});
+        error_window.cancelEnabled = false;
+        error_window.show();
         return;
     }
 
@@ -128,18 +143,22 @@ function determine_shape(triangle_shape, origin_x, origin_y, origin_z, height, w
 
         // Make sure the height/width are valid.
         if ((origin_y > 32 - height || horizontal > 32 - origin_x || horizontal > 16 + origin_x) && Format.canvas_limit) {
-            var error_window5 = new Dialog({title: 'Error', id: 'error_window_5', lines: [
-                'Error occurred:<br\>' +
-                'Your triangle would go outside of the restricted area.<br/>'+
-                ' <br\>' +
-                'Please make sure to keep the triangle small enough so that it still fits in the canvas.'
-            ], draggable: true, onConfirm() {
-                error_window5.hide();
-                triangleWindow();
-            }
-            });
-            error_window5.show();
-            return;
+	        var error_window = new Dialog(
+	        	{
+	            	id: 'error_window', 
+	            	title: 'Error', 
+	            	lines: [
+		                'Error occurred:\n',
+		                'Your triangle would go outside of the restricted area.\n',
+		                'Please make sure to keep the triangle within the canvas.'
+		            ],
+		            onConfirm() {
+		                error_window.hide();
+		            }
+	        	});
+	        error_window.cancelEnabled = false;
+	        error_window.show();
+	        return;
         }
 
         // Generate the triangle.
@@ -154,17 +173,21 @@ function determine_shape(triangle_shape, origin_x, origin_y, origin_z, height, w
 
         // Make sure the height/width are valid.
         if ((origin_y > 32 - height || horizontal > 16 + origin_x) && Format.canvas_limit) {
-            var error_window8 = new Dialog({title: 'Error', id: 'error_window_8', lines: [
-                'Error occurred:<br\>' +
-                'Your triangle would go outside of the restricted area.<br/>'+
-                ' <br\>' +
-                'Please make sure to keep the triangle small enough so that it still fits in the canvas.'
-            ], draggable: true, onConfirm() {
-                error_window8.hide();
-                triangleWindow();
-            }
-            });
-            error_window8.show();
+	        var error_window = new Dialog(
+	        	{
+	            	id: 'error_window', 
+	            	title: 'Error', 
+	            	lines: [
+		                'Error occurred:\n',
+		                'Your triangle would go outside of the restricted area.\n',
+		                'Please make sure to keep the triangle within the canvas.'
+		            ],
+		            onConfirm() {
+		                error_window.hide();
+		            }
+	        	});
+	        error_window.cancelEnabled = false;
+	        error_window.show();
             return;
         }
 
@@ -188,43 +211,39 @@ function determine_shape(triangle_shape, origin_x, origin_y, origin_z, height, w
 
         // Make sure the angle is a full step of 22.5 degrees.
         if (Math.round(angle_steps) != angle_steps) {
-            var error_window3 = new Dialog({title: 'Error', id: 'error_window_3', lines: [
-                'Error occurred:<br\>' +
-                'Angle must be a multiple of 22.5 degrees.<br/>'
-            ], draggable: true, onConfirm() {
-                error_window3.hide();
-                triangleWindow();
-            }
-            });
-            error_window3.show();
-            return;
-        }
-        // Make sure the angle is between 0 and 90 degrees.
-        if (angle_steps < 1 || angle_steps > 3) {
-            var error_window4 = new Dialog({title: 'Error', id: 'error_window_4', lines: [
-                'Error occurred:<br\>' +
-                'Angle must be greater than 0 degrees and less than 90 degress.<br/>'
-            ], draggable: true, onConfirm() {
-                error_window4.hide();
-                triangleWindow();
-            }
-            });
-            error_window4.show();
+	        var error_window = new Dialog(
+	        	{
+	            	id: 'error_window', 
+	            	title: 'Error', 
+	            	lines: [
+		                'Error occurred:\n',
+		                'Pyramids require angles to be a multiple of 22.5 between 0 and 90 (exclusive).',
+		            ],
+		            onConfirm() {
+		                error_window.hide();
+		            }
+	        	});
+	        error_window.cancelEnabled = false;
+	        error_window.show();
             return;
         }
         // Make sure the height/width are valid.
         if ((origin_y > 32 - height - yOffset || horizontal > 32 - origin_x - xOffset || horizontal > 16 + origin_x - xOffset || horizontal > 32 - origin_z - xOffset || horizontal > 16 + origin_z - xOffset) && Format.canvas_limit) {
-            var error_window9 = new Dialog({title: 'Error', id: 'error_window_9', lines: [
-                'Error occurred:<br\>' +
-                'Your triangle would go outside of the restricted area.<br/>'+
-                ' <br\>' +
-                'Please make sure to keep the triangle small enough so that it still fits in the canvas.'
-            ], draggable: true, onConfirm() {
-                error_window9.hide();
-                triangleWindow();
-            }
-            });
-            error_window9.show();
+	        var error_window = new Dialog(
+	        	{
+	            	id: 'error_window', 
+	            	title: 'Error', 
+	            	lines: [
+		                'Error occurred:\n',
+		                'Your triangle would go outside of the restricted area.\n',
+		                'Please make sure to keep the triangle within the canvas.'
+		            ],
+		            onConfirm() {
+		                error_window.hide();
+		            }
+	        	});
+	        error_window.cancelEnabled = false;
+	        error_window.show();
             return;
         }
 
@@ -308,9 +327,6 @@ function generatePoint(height, width, angle, axis, tilt, granularity, triType) {
 
     buildCube(fromX,fromY,fromZ,toX,toY,toZ,origin,axis,tilt,angled_point);
 
-
-
-
     Canvas.updateAll();
 
     return angled_point;
@@ -335,9 +351,6 @@ function buildCube(fromX,fromY,fromZ,toX,toY,toZ,origin,axis,rotation,group) {
     }).addTo(group).init()
 }
 
+
 })()
 
-onUninstall = function() {
-    //Removing entries
-    Blockbench.removeMenuEntry('Generate triangle')
-};
