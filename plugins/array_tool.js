fcad17d03b470e7edd561a96720eb0ab7158361f
Malik12tree <82341209+Malik12tree@users.noreply.github.com>
2021-07-11 18:08:17 +0300

    Update array_tool.js

--- plugins/array_tool.js
+++ plugins/array_tool.js
@@ -1,3 +1,5 @@
+
+
 (function() {
     var open_menu;
 
@@ -22,12 +24,11 @@
                             array_count: {label: 'Array Count', type: "number", value: 1, min: 0, step: 1},
                             array_offset: {label: "Array Offset", type: "vector", value: [0,0,0], step: 0.25},
                         },
-                        lines: ["<p>Note: Offset Values From A Range Of -0.99 To 0.99 Will Cause Cube Overlapping.</p>"],
-                        onConfirm: function(formResult) {
+                        onConfirm: function (formResult) {
                             array_dialog.hide();
                                 for (var m = 0; m < formResult.array_count; m++) {
-                                    array_confirm(formResult.array_offset, m);
                                 }
+                                array_confirm(formResult.array_offset, m);
                         }
                     }).show()
                 }
@@ -37,7 +38,6 @@
                         icon: 'error_outline',
                         message: 'You must select at least one cube!',
                         buttons: ['ok']
-            
                     });
                 }
             }
@@ -50,68 +50,73 @@
         
     });
 
-})();   
+})();
+
+
+
+
 
 function array_confirm(array_offset, m) {
+    copy_group = new Group('Array').init()
     var added_elements = [];
-    Undo.initEdit({
-        elements: added_elements, 
-        outliner: true, 
-        selection: true,
-    })
-    selected.forEachReverse(selected_cubes => {
-        var copy = new Cube({
-            name: `${selected_cubes.name}_array`,
-            rotation: selected_cubes.rotation,
-            origin: [selected_cubes.origin[0] + array_offset[0] + m * array_offset[0],selected_cubes.origin[1] + array_offset[1] + m * array_offset[1],selected_cubes.origin[2] + array_offset[2] + m * array_offset[2]],
-            to:[selected_cubes.to[0] + array_offset[0] + m * array_offset[0], selected_cubes.to[1] + array_offset[1] + m * array_offset[1], selected_cubes.to[2] + array_offset[2] + m * array_offset[2]], 
-            from:[selected_cubes.from[0] + array_offset[0] + m * array_offset[0], selected_cubes.from[1] + array_offset[1] + m * array_offset[1], selected_cubes.from[2] + array_offset[2] + m * array_offset[2]],
-            color: selected_cubes.color,
-            faces: {
-                south: {
-                    uv: selected_cubes.faces.south.uv,
-                    texture: selected_cubes.faces.south.texture,
-                    rotation: selected_cubes.faces.south.rotation,
-                    cullface: selected_cubes.faces.south.cullface
-                },
-                north: {
-                    uv: selected_cubes.faces.north.uv,
-                    texture: selected_cubes.faces.north.texture,
-                    rotation: selected_cubes.faces.north.rotation,
-                    cullface: selected_cubes.faces.north.cullface
-                },
-                east: {
-                    uv: selected_cubes.faces.east.uv,
-                    texture: selected_cubes.faces.east.texture,
-                    rotation: selected_cubes.faces.east.rotation,
-                    cullface: selected_cubes.faces.east.cullface
-                },
-                west: {
-                    uv: selected_cubes.faces.west.uv,
-                    texture: selected_cubes.faces.west.texture,
-                    rotation: selected_cubes.faces.west.rotation,
-                    cullface: selected_cubes.faces.west.cullface
-                },
-                down: {
-                    uv: selected_cubes.faces.down.uv,
-                    texture: selected_cubes.faces.down.texture,
-                    rotation: selected_cubes.faces.down.rotation,
-                    cullface: selected_cubes.faces.down.cullface
-                },
-                up: {
-                    uv: selected_cubes.faces.up.uv,
-                    texture: selected_cubes.faces.up.texture,
-                    rotation: selected_cubes.faces.up.rotation,
-                    cullface: selected_cubes.faces.up.cullface
+    Undo.initEdit({elements: added_elements, outliner: true, selection: true});
+    for (var i = 0; i < m; i++) {
+        selected.forEach(selected_cubes => {
+            var copy = new Cube({
+                name: `${selected_cubes.name}_array`,
+                rotation: selected_cubes.rotation,
+                origin: [selected_cubes.origin[0] + array_offset[0] + i * array_offset[0],selected_cubes.origin[1] + array_offset[1] + i * array_offset[1],selected_cubes.origin[2] + array_offset[2] + i * array_offset[2]],
+                to:[selected_cubes.to[0] + array_offset[0] + i * array_offset[0], selected_cubes.to[1] + array_offset[1] + i * array_offset[1], selected_cubes.to[2] + array_offset[2] + i * array_offset[2]], 
+                from:[selected_cubes.from[0] + array_offset[0] + i * array_offset[0], selected_cubes.from[1] + array_offset[1] + i * array_offset[1], selected_cubes.from[2] + array_offset[2] + i * array_offset[2]],
+                color: selected_cubes.color,
+                inflate: selected_cubes.inflate,
+                mirror_uv: selected_cubes.mirror_uv,
+                shade: selected_cubes.shade,
+                faces: {
+                    south: {
+                        uv: selected_cubes.faces.south.uv,
+                        texture: selected_cubes.faces.south.texture,
+                        rotation: selected_cubes.faces.south.rotation,
+                        cullface: selected_cubes.faces.south.cullface
+                    },
+                    north: {
+                        uv: selected_cubes.faces.north.uv,
+                        texture: selected_cubes.faces.north.texture,
+                        rotation: selected_cubes.faces.north.rotation,
+                        cullface: selected_cubes.faces.north.cullface
+                    },
+                    east: {
+                        uv: selected_cubes.faces.east.uv,
+                        texture: selected_cubes.faces.east.texture,
+                        rotation: selected_cubes.faces.east.rotation,
+                        cullface: selected_cubes.faces.east.cullface
+                    },
+                    west: {
+                        uv: selected_cubes.faces.west.uv,
+                        texture: selected_cubes.faces.west.texture,
+                        rotation: selected_cubes.faces.west.rotation,
+                        cullface: selected_cubes.faces.west.cullface
+                    },
+                    down: {
+                        uv: selected_cubes.faces.down.uv,
+                        texture: selected_cubes.faces.down.texture,
+                        rotation: selected_cubes.faces.down.rotation,
+                        cullface: selected_cubes.faces.down.cullface
+                    },
+                    up: {
+                        uv: selected_cubes.faces.up.uv,
+                        texture: selected_cubes.faces.up.texture,
+                        rotation: selected_cubes.faces.up.rotation,
+                        cullface: selected_cubes.faces.up.cullface
+                    }
                 }
-            }
-        }).init()
-        added_elements.push(copy);
-    })
+            }).addTo(copy_group).init();
+            added_elements.push(copy);
+        })
+    }
+    Undo.finishEdit('Created Array', {elements: added_elements, outliner: true, selection: true});
     BarItems.move_tool.select();
-    Undo.finishEdit('Created Array', {elements: added_elements, outliner: true, selection: true,});
 }
-
 
 /*
  */
