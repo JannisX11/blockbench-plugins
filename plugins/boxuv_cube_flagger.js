33f615933942b9ccce07586d18c5bfc96190d9ae
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-05-21 18:05:33 -0400

    Update boxuv_cube_flagger.js

--- plugins/boxuv_cube_flagger.js
+++ plugins/boxuv_cube_flagger.js
@@ -11,7 +11,8 @@
     tags: ['Textures', 'BoxUV', 'UV'],
     about: 'Have you ever been using BoxUV and had to change to Per-Face UV because your cubes were smaller than one unit on an axis? You can use the BoxUV Cube Flagger plugin to flash all cubes smaller than one unit, allowing you easily find and edit them so that you can keep using BoxUV. Simple, yet effective.\n\nTo use this plugin, go to \'File > Plugins > Available\' and search for \'BoxUV Cube Flagger\'. Click install, then use \'Tools > Flag Invalid Cubes\'. This will cause cubes less than one unit on any axis to blink on-screen.\n\nIt would be appreciated to report any bugs and suggestions!',
     onload() {
-      const flashMaterial = new THREE.MeshBasicMaterial({color: 0xFF3F3F})
+      const flashMaterial = new THREE.MeshBasicMaterial({color: 0xFF3F3F});
+      let running, cubes;
       button = new Action('flag_invalid_cubes', {
         name: 'Flag Invalid Cubes',
         description: 'Highlight cubes less than 1 unit',
@@ -20,10 +21,15 @@
           const highlighter = {
             i: 0,
             start: function() {
-              for (const cube of Cube.all) {
-                if (cube.size(0) < 1 || cube.size(1) < 1 || cube.size(2) < 1) {
-                  cube.mesh.material_non_flash = cube.mesh.material;
-                }
+              if (running) {
+                console.log("already running...")
+                return;
+              }
+              console.log("running")
+              running = true;
+              cubes = Cube.all.filter(cube => (cube.size(0) > 0 && cube.size(0) < 1) || (cube.size(1) > 0 && cube.size(1) < 1) || (cube.size(2) > 0 && cube.size(2) < 1));
+              for (const cube of cubes) {
+                cube.mesh.material_non_flash = cube.mesh.material;
               };
               clearInterval(highlighter.interval);
               highlighter.i = 0;
@@ -33,11 +39,11 @@
             flash: function() {
               var fc = highlighter.i;
               if (fc > 5) {
-                x = 0;
+                running = false;
                 clearInterval(highlighter.interval);
               };
-              for (const cube of Cube.all) {
-                if (cube.size(0) < 1 || cube.size(1) < 1 || cube.size(2) < 1) {
+              for (const cube of cubes) {
+                if (cube.mesh) {
                   cube.mesh.material = (fc % 2) ? flashMaterial : cube.mesh.material_non_flash;
                 }
               };
