ac04fdcf4135a57cec004f82018a8702a3cf6bc2
Malik12tree <82341209+Malik12tree@users.noreply.github.com>
2022-06-03 02:29:56 +0300

    Pie Menus Plugin

---
+++ plugins/pie_menu.js
@@ -0,0 +1,420 @@
+(function() {
+    class PieMenu extends BarItem{
+        /**
+         * @type PieMenu
+         */
+        static active = null;
+        static all = {};
+        static TWO_PI = 6.283185307179586;
+        static HALF_PI = 1.5707963267948966;
+        static QUARTER_PI = 0.7853981633974483;
+        static mouseEvent = null;
+
+        /**
+         * Create Pie Menu
+         * @param {{
+         * name: String,
+         * id: String,
+         * radius: Number,
+         * structure: Array<Action | String>,
+         * condition: any,
+         * keybind: {key:Number,shift:boolean,ctrl:boolean,alt:boolean,meta:boolean}
+         * }} data 
+         */
+        constructor(id, data){
+            if (typeof id == 'object') {
+                data = id;
+                id = data.id;
+            }
+            super(id, data);
+            this.radius = data.radius !== undefined ? data.radius: 200;
+            this.structure = data.structure || [];
+            this.node = $(`<div class=pieMenu><div class=pieSlices></div><h4></h4><div class=pieCenter><svg><defs><mask id=AngleIndicator><circle/></mask></defs><foreignObject mask=url(#AngleIndicator)><div class=gradient xmlns=http://www.w3.org/1999/xhtml /></foreignObject></svg></div></div>`);
+
+            this.uuid = guid();
+            PieMenu.all[this.uuid] = this;
+
+            this.setName(this.name);
+
+            this.cache = {
+                isHold: false,
+                lastCenter: [0,0]
+            }
+
+            if (this.id) this.setId(this.id);
+        }
+        equals(otherPieMenu) {
+            return this.uuid == otherPieMenu.uuid;
+        }
+        setName(value) {
+            this.name = value;
+            this.setNodeTitle(value);
+        }
+        setNodeTitle(value){
+            this.node.children('h4').text(value);
+        }
+        setId(value){
+            this.id = value;
+            this.node.attr('id', value);
+            return this;
+        }
+        conditionMet(){
+            return Condition(this.condition);
+        }
+        show(x = mouse_pos.x, y = mouse_pos.y){
+            if (Project==0||Format==0) return;
+            if (!this.conditionMet()) return;
+            if (PieMenu.active && this.equals(PieMenu.active)) return;
+            if (PieMenu.active) PieMenu.active.hide();
+            PieMenu.active = this;
+            this.build();
+
+            x = Math.clamp(x, this.radius, window.innerWidth - this.radius);
+            y = Math.clamp(y, this.radius, window.innerHeight - this.radius);
+
+            this.node[0].style.left = x + 'px';
+            this.node[0].style.top =  y + 'px';
+            this.node[0].style.width = `calc(${this.radius<<1}px + calc(var(--pie-major_radius)*2))` 
+            this.node[0].style.height = `calc(${this.radius<<1}px + calc(var(--pie-major_radius)*2))` 
+            this.cache.lastCenter = [x,y];
+
+            $(document.body).append(this.node)
+        }
+        hide(){
+            PieMenu.active = undefined;
+            this.node.remove();
+        }
+
+        // \ - - - /
+        getActionAt(i){
+            return typeof this.structure[i] == 'string' ? BarItems[this.structure[i]]: this.structure[i];
+        }
+        actionConditionMetAt(i) {
+            return Condition(this.getActionAt(i).condition);
+        }
+        actionIndexFromAngle(angle, unit = 'rad'){
+            if (unit == 'deg') {
+                angle = Math.degToRad(angle);
+            }
+            return Math.floor( (angle / PieMenu.TWO_PI) * this.structure.length);
+        }
+        getActionFromAngle(angle, unit = 'rad'){
+            let index = this.actionIndexFromAngle(angle, unit);
+            return this.getActionAt(index);
+        }
+        // \ - - - /
+
+        build(){
+            let slices = this.node.children('.pieSlices');
+            slices.empty();
+
+            let angleStep = 360 / this.structure.length;
+            let angle = -angleStep-90;
+
+            for (let i = 0; i < this.structure.length; i++) {
+                angle += angleStep;
+                const action = this.getActionAt(i);
+
+                // Fix conditions with tools
+                if (action instanceof Tool) {
+                    let index = Toolbox.children.findIndex(t => t.id == action.id);
+                    if (index >= 0) {
+                        Toolbox.condition_cache[i] = Condition(action.condition);
+                    }
+                    Toolbox.update()
+                }
+
+                if (!this.actionConditionMetAt(i)) continue;
+                
+                // Create Pie Slice Which Handels Rotation And Animation ( Action Container )
+                let slice = $(`<div class=pieSlice></div>`);
+
+                slice.attr('style', `transform: translate(-50%,-50%) rotate(${angle}deg);`);
+
+                // Create Action Node
+                let actionNode = $('<div></div>');
+
+                // Add Icon
+                if (action.icon) {
+                    let iconParms = action.icon.split(' ');
+                    actionNode.append(Blockbench.getIconNode(iconParms[0], action.color || iconParms[1]));
+                }
+
+                // Add Text
+                actionNode.append($('<span></span>').text(tl(action.name)));
+                
+                // Add Keybind Label
+                actionNode.append( $(`<label class=keybinding_label i=${i}></label>`).text( i < 10 ?i:'' ) );
+
+                // Reverse Rotation
+                actionNode.attr('style', `transform: rotate(${360-angle}deg);`);
+
+                slice.append(actionNode);
+                slices.append(slice);
+
+            }
+
+            return this.node;
+        }
+        trigger(event){
+            this.cache.isHold = event.repeat;
+            if (!this.cache.isHold) {
+                this.setNodeTitle(this.name + ' ( State: Single )')
+                this.show();
+            } else {
+                this.setNodeTitle(this.name + ' ( State: Hold )')
+            }
+        }
+        unTrigger(...arg){ // is it even a word?
+
+            let action;
+
+            if (typeof arg[0] === 'number') {
+                if (this.actionConditionMetAt(arg[0])) {
+                    this.hide();
+                    action = this.getActionAt(arg[0]);
+                }
+            } else if (arg[0] || this.cache.isHold) {
+                let angle = this.angleToCursor() - (Math.PI - PieMenu.QUARTER_PI/2);
+                angle = (angle + PieMenu.TWO_PI) % PieMenu.TWO_PI;
+
+                action = this.getActionFromAngle(angle);
+                
+                this.hide();
+            }
+
+            if (action) {
+                if (Condition(action.condition)) {
+                    if (action.click) {
+                        action.click();
+                    }
+                    if (action.select) {
+                        action.select();
+                    }
+                    if (action.children) {
+                        this.showActionMenuOf(action);
+                    }
+                }
+            }
+        }
+        showActionMenuOf(action){
+            if (typeof action.children == 'function') {
+                new Menu(action.children()).show(PieMenu.mouseEvent);
+
+            } else if (action.children instanceof Array && action.children.length) {
+                new Menu(action.children).show(PieMenu.mouseEvent);
+            }
+        }
+
+        delete() {
+            var scope = this;
+            this.toolbars.forEach(bar => {
+                bar.remove(scope);
+            })
+            delete BarItems[this.id];
+            Keybinds.actions.remove(this);
+            delete PieMenu.all[this.uuid];
+        }
+        angleToCursor(){
+            return new THREE.Vector2( mouse_pos.x , mouse_pos.y ).sub(new THREE.Vector2( ...this.cache.lastCenter )).angle();
+        }
+        static initCSS() {
+            let style = $('#pieMenuStyles');
+            if (style.length) {
+                style.remove();   
+            }
+            style = $('<style id="pieMenuStyles"></style>');
+            style.text(
+                // Container
+                `:root{--pie-dir_indicator_angle_padding:10%;--pie-major_radius:30px;--pie-major_thickness:22px}.pieMenu{transform:translate(-50%,-50%);width:50%;height:50%;position: fixed;}.pieCenter{position:relative;top:50%;left:50%;transform:translate(-50%,-50%);width:calc(var(--pie-major_radius) * 2);height:calc(var(--pie-major_radius) * 2)}.pieCenter>svg{height:calc(var(--pie-major_radius) * 2);width:calc(var(--pie-major_radius) * 2)}.pieCenter>svg circle{cx:var(--pie-major_radius);cy:var(--pie-major_radius);r:calc(var(--pie-major_radius) - calc(var(--pie-major_thickness) * 0.5));stroke-width:calc(var(--pie-major_thickness) * .5);fill:none;stroke:white}.pieCenter div.gradient{width:100%;height:100%;border-radius:50%;--l_o:calc(50% - var(--pie-dir_indicator_angle_padding));--r_o:calc(50% + var(--pie-dir_indicator_angle_padding));background:conic-gradient(from 0deg at 50% 50%,var(--color-bright_ui) var(--l_o),var(--color-accent) var(--l_o),var(--color-accent) 50%,var(--color-accent) var(--r_o),var(--color-bright_ui) var(--r_o));box-shadow: inset 0px 0px 20px 0px black;}.pieCenter>svg foreignObject{--r:calc(var(--pie-major_radius)*2);width:var(--r);height:var(--r)}`
+                +
+                // Actions
+                `.pieSlice{height:100%;width:fit-content;position: absolute;left: 50%;top: 50%;animation:expandOut .1s linear}.pieSlice div{display:flex;height:30px;padding:4px;padding-left:34px;padding-right:8px;width:max-content;background-color: var(--color-bright_ui);color: var(--color-bright_ui_text);border-radius:5px;box-shadow: 0 0 6px 0px #0005;}.pieSlice div.focused{background-color:var(--color-accent)}.pieSlice div>i{margin-top:1px;margin-right:4px;margin-left:-28px;pointer-events:none}.pieSlice div>img{cursor:default;height:20px;width:20px;color:var(--color-text);white-space:nowrap;margin-bottom:-3px;margin-left:-27px;margin-right:5px;margin-top:1px}.pieSlice div>span{pointer-events:none;flex:1 0 auto}.pieSlices{width:100%;height:100%;position: absolute;}`
+                +
+                // Animations
+                `@keyframes expandOut{0%{height:0%}100%{height:100%}}`
+                + 
+                // Title
+                `.pieMenu h4{position: absolute;left: 50%;top: 37.5%;transform: translate(-50%,-50%);width: fit-content;color: var(--color-subtle_text);}`
+            )
+            $(document.body).append(style);
+        }
+        static onkeyup(e){
+            if (!PieMenu.active) return;
+            if (!PieMenu.active.keybind) return;
+            if (e.which == 27) { PieMenu.active.hide(); return; };
+
+            let actionIndex = e.which - 48;
+            if (Math.isBetween(actionIndex, 0, 9)) {
+                PieMenu.active.unTrigger(actionIndex);
+                return;
+            }
+            if (PieMenu.active.cache.isHold && e.which == PieMenu.active.keybind.key) {
+                PieMenu.active.unTrigger(); 
+            }
+        }
+        static onclick(e){
+            if (!PieMenu.active) return;
+            if (!PieMenu.active.cache.isHold) {
+                PieMenu.active.unTrigger(true);
+            }
+        }
+        static onmousemove(e){
+            PieMenu.mouseEvent = e;
+            if (!PieMenu.active) return;
+            
+            let pieCenter = PieMenu.active.node.children('.pieCenter');
+
+            let angle = PieMenu.active.angleToCursor();
+            pieCenter.attr('style', `transform: translate(-50%, -50%) rotate(calc(${angle}rad - 90deg))`);
+
+            angle -= Math.PI - PieMenu.QUARTER_PI/2;
+            angle = (angle + PieMenu.TWO_PI) % PieMenu.TWO_PI;
+            
+            let index = PieMenu.active.actionIndexFromAngle(angle);
+            PieMenu.active.node.find('.pieSlices div').removeClass('focused');
+            if (PieMenu.active.actionConditionMetAt(index)) {
+                
+                PieMenu.active.node
+                .children('.pieSlices')
+                .find(`label[i=${index}]`)
+                .parent()
+                .addClass('focused')
+            }
+        }
+        fromAction(action, menu={}) {
+            if (typeof action.children == 'function') {
+                this.structure = action.children();
+            } else {
+                this.structure = action.children;
+            }
+            this.condition = action.condition || menu.condition;
+            this.structure = this.structure.filter(e=> e != '_');
+            this.name = tl(action.name);
+            return this;
+        }
+        fromBarSelect(barselect){
+            let actions  = [];
+            for (const key in barselect.options) {
+                const option = barselect.options[key];
+                
+                let name = barselect.getNameFor(key)
+                let icon = option.icon || '';
+                let data = {
+                    name,icon,
+                    click(){
+                        barselect.set(key);
+                        barselect.onChange(barselect.value);
+                    },
+                    condition: undefined
+                };
+                if (option.condition) data.condition = option.condition;
+                actions.push( new Action(data) )
+            }
+            this.structure=actions;
+            return this;
+        }
+        
+    }
+    window.PieMenu = PieMenu;
+
+    document.removeEventListener('keyup', PieMenu.onkeyup)
+    document.addEventListener('keyup', PieMenu.onkeyup);
+
+    document.removeEventListener('click', PieMenu.onclick)
+    document.addEventListener('click', PieMenu.onclick);
+
+    document.removeEventListener('mousemove', PieMenu.onmousemove)
+    document.addEventListener('mousemove', PieMenu.onmousemove);
+
+    PieMenu.initCSS();
+
+    let addedPieMenus = [];
+    Plugin.register("pie_menu", {
+		title: "Pie Menus",
+		icon: "fas.fa-chart-pie",
+		author: "Malik12tree",
+		description: "Add pie shaped menus for faster work.",
+		about: "<h1>Built-in Pies</h1><style>.piePrivate p{width: fit-content;display: inline;margin-left: 10px !important;}.piePrivate tr td:nth-child(2){padding-left:10px}</style><table class=piePrivate><tr>    <td>Add Object Pie</td>    <td>Keybind: </td>    <td><p><code>shift + a</code></p></td></tr><tr>    <td>Import Pie</td>    <td>Keybind: </td>    <td><p><code>shift + q</code></p></td></tr><tr>    <td>Export Pie</td>    <td>Keybind: </td>    <td><p><code>shift + w</code></p></td></tr><tr>    <td>Mode Pie</td>    <td>Keybind: </td>    <td><p><code>shift + tab (web: shift + x)</code></p></td></tr><tr>    <td>View Mode Pie</td>    <td>Keybind: </td>    <td><p><code>shift + z</code></p></td></tr><tr>    <td>Toolbox Pie</td>    <td>Keybind: </td>    <td><p><code>shift + t</code></p></td></tr><tr>    <td>New Pie</td>    <td>Keybind: </td>    <td><p><code>shift + n</code></p></td></tr><tr>    <td>Preferences Pie</td>    <td>Keybind: </td>    <td><p><code>shift + o</code></p></td></tr><tr>    <td>Rotate Pie</td>    <td>Keybind: </td>    <td><p><code>shift + r</code></p></td></tr><tr>    <td>Flip Pie</td>    <td>Keybind: </td>    <td><p><code>shift + f</code></p></td></tr><tr>    <td>Center Pie</td>    <td>Keybind: </td>    <td><p><code>shift + c</code></p></td></tr><tr>    <td>Properties Pie</td>    <td>Keybind: </td>    <td><p><code>shift + p</code></p></td></tr></table>",
+		version: "1.0.0",
+		variant: "both",
+        tags: ["Interface"],
+		onload() {
+            addedPieMenus.push(
+                new PieMenu({
+                    name: "Add", // NEEDS LANG SUPPORT
+                    keybind: {key:65,shift:true},
+                    structure: ['add_cube','add_group','add_locator','add_mesh','add_null_object','add_texture_mesh','add_animation','add_keyframe']
+                }),
+
+                new PieMenu({
+                    keybind: {key:81,shift:true}
+                }).fromAction(MenuBar.menus.file.structure.find(e=>e.id=='import')),
+                
+                new PieMenu({
+                    keybind: {key:87,shift:true},
+                }).fromAction(MenuBar.menus.file.structure.find(e=>e.id=='export')),
+                
+                new PieMenu({
+                    keybind: {key:90,shift:true},
+                }).fromBarSelect(BarItems['view_mode']),
+                
+                new PieMenu({
+                    name: "Mode", // NEEDS LANG SUPPORT
+                    keybind: Blockbench.isWeb ? {key:88,shift:true}: {key:9,shift:true},
+                    structure: (function() {
+                        let actions = [];
+                        for (const key in Modes.options) {
+                            const mode = Modes.options[key];
+
+                            actions.push(
+                                new Action({
+                                    name: mode.name,
+                                    condition: mode.condition,
+                                    click(){ mode.select() }
+                                })
+                            )
+                        }
+                        return actions;
+                    })()
+                }),
+
+                new PieMenu({
+                    keybind: {key:84,shift:true},
+                }).fromAction(MenuBar.menus.filter.structure.find(e=>e.id=='main_tools')),
+
+                new PieMenu({
+                    keybind: {key:78,shift:true},
+                }).fromAction(MenuBar.menus.file.structure.find(e=>e.id=='new')),
+
+                new PieMenu({
+                    keybind: {key:79,shift:true},
+                }).fromAction(MenuBar.menus.file.structure.find(e=>e.id=="preferences")),
+
+                ...(function() {
+                    let pies = [];
+                    const keybinds = [
+                        {key:82,shift:true},
+                        {key:70,shift:true},
+                        {key:67,shift:true},
+                        {key:80,shift:true}
+                    ];
+                    for (let i = 1; i < 5; i++) {
+                        pies.push(
+                            new PieMenu({
+                                keybind: keybinds[i-1]
+                            }).fromAction(MenuBar.menus.transform.structure[i], MenuBar.menus.transform),
+                        )
+                    }
+                    return pies;
+                })(),
+            );
+            addedPieMenus[7].structure.push(BarItems['plugins_window']); // add plugins window in the preferences pie 
+
+        },
+        onunload() {
+            addedPieMenus.forEach(m => m.delete() );
+            addedPieMenus = [];
+        }
+    });
+})()
