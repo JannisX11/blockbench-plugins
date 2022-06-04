45f63e1e1582ba3958edb040c55a9d1ca51d1ab1
Manuel-3 <58179488+Manuel-3@users.noreply.github.com>
2022-06-04 17:14:23 +0200

    Keep offset

--- plugins/quick_box_uv_layout.js
+++ plugins/quick_box_uv_layout.js
@@ -16,15 +16,25 @@
                 click: function () {
                     Undo.initEdit({ elements: Cube.selected });
 
+                    // Horizontal and vertical offset
+                    let h = 0;
+                    let v = 0;
+
+                    // If box-uv then convert to per-face
                     if (Project.box_uv) Project.box_uv = false;
 
                     Cube.selected.forEach(cube => {
-                        const h = cube.uv_offset[0];
-                        const v = cube.uv_offset[1];
+                        // Get cube size
                         const x = cube.size(0, true);
                         const y = cube.size(1, true);
                         const z = cube.size(2, true);
 
+                        // Get the required offset by looking at the top left corner of the up face
+                        h = cube.faces['up'].uv[2] - z;
+                        v = cube.faces['up'].uv[3];
+
+                        // Set individual faces position and size
+                        // Note that up face is flipped on both axes and down face is flipped on the horizontal axis
                         cube.faces['north'].uv = [h + z, v + z, h + z + x, v + z + y];
                         cube.faces['east'].uv = [h, v + z, h + z, v + z + y];
                         cube.faces['west'].uv = [h + z + x, v + z, h + z + x + z, v + z + y];
@@ -33,6 +43,7 @@
                         cube.faces['down'].uv = [h + z + x + x, v, h + z + x, v + z];
                     });
                     Undo.finishEdit('convert to box-uv layout');
+                    // Update view to make the changes show
                     Canvas.updateView({elements: Cube.selected, element_aspects: {uv: true}});
                 }
             });
