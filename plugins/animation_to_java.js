9193261f86a534572febfeb13004b8a8256bfda4
VincentHuto <amae804@gmail.com>
2022-07-19 00:42:47 -0500

    Removed unused code

--- plugins/animation_to_java.js
+++ plugins/animation_to_java.js
@@ -37,19 +37,6 @@
 })();
 
 function generateFile() {
-  const getCircularReplacer = () => {
-    const seen = new WeakSet();
-    return (key, value) => {
-      if (typeof value === "object" && value !== null) {
-        if (seen.has(value)) {
-          return;
-        }
-        seen.add(value);
-      }
-      return value;
-    };
-  };
-
   let outfileText = "";      
 
   for (const animation of Animation.all) {
