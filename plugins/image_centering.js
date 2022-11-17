52253f10487a7bf84f4b9cec7366ebddd3064006
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-11-16 19:19:05 -0500

    Update min version

--- plugins/image_centering.js
+++ plugins/image_centering.js
@@ -17,7 +17,7 @@
 		about: "This plugin adds a button that allows you to reset your viewport in the Image format.\n## How to use\nTo use this plugin, go to the brush toolbar (top of the image mode) and click the `Center Image Viewport` button. If it doesn't show up, make sure you are in the Image format. If you are, you can add the tool using the `Customize Toolbar` option. There will be a confirmation message displayed on-screen once you center the viewport.\n\nPlease report any bugs or suggestions you may have.",
 		tags: ["Customization", "Format: Image", "UX"],
 		version: "1.0.0",
-		min_version: "4.2.0",
+		min_version: "4.4.0",
 		variant: "both",
 		oninstall: () => showAbout(true),
 		onload() {
