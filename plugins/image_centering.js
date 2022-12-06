67c13bece9ce4aff7c8ac2cb65629c22ea10d558
SirJain <94301223+SirJain0@users.noreply.github.com>
2022-12-06 15:48:29 -0500

    Update file

--- plugins/image_centering.js
+++ plugins/image_centering.js
@@ -9,6 +9,7 @@
 		twitter: "https://twitter.com/SirJain2",
 		discord: "https://discord.gg/wM4CKTbFVN"
 	}
+
 	Plugin.register(id, {
 		title: name,
 		icon,
@@ -16,23 +17,21 @@
 		description: "Adds a button that centers the image viewport in an Image format.",
 		about: "This plugin adds a button that allows you to reset your viewport in the Image format.\n## How to use\nTo use this plugin, go to the brush toolbar (top of the image mode) and click the `Center Image Viewport` button. If it doesn't show up, make sure you are in the Image format. If you are, you can add the tool using the `Customize Toolbar` option. There will be a confirmation message displayed on-screen once you center the viewport.\n\nPlease report any bugs or suggestions you may have.",
 		tags: ["Format: Image", "UX"],
-		version: "1.0.0",
+		version: "1.1.0",
 		min_version: "4.4.0",
 		variant: "both",
 		oninstall: () => showAbout(true),
 		onload() {
 			addAboutButton();
 			Blockbench.showQuickMessage("Successfully installed Image Centering plugin!", 2000);
+			document.addEventListener("keydown", keyAction)
+			
 			button = new Action("image_center_button", {
 				name: 'Center Image Viewport',
 				icon: 'center_focus_strong',
 				description: 'Center the viewport of your image',
 				condition: () => Format?.id == "image",
-				click() {
-					CenterViewport()
-					SetZoom()
-					Blockbench.showQuickMessage("Centered viewport!", 2000)
-				}
+				click: () => Center()
 			})
 
 			Toolbars.brush.add(button);
@@ -42,10 +41,27 @@
 			button.delete();
 			MenuBar.removeAction(`help.about_plugins.about_${id}`);
 			Blockbench.showQuickMessage("Uninstalled Image Centering plugin", 2000);
+			document.removeEventListener("keydown", keyAction)
 		}
 	})
 
-	function CenterViewport() {
+	function keyAction(event) {
+		const keyName = event.key;
+		
+		if (Format?.id === "image") {
+			if (keyName === "c") {
+				Center();
+			}
+		}
+	}
+
+	function Center() {
+		SetViewport();
+		SetZoom();			
+		Blockbench.showQuickMessage("Centered viewport!", 2000);
+	}
+
+	function SetViewport() {
 		let uv_viewport = UVEditor.vue.$refs.viewport;
 		if (!uv_viewport) return;
 		UVEditor.setZoom(Project.uv_viewport.zoom);
@@ -63,6 +79,7 @@
 		Vue.nextTick(() => {
 			if (Painter.selection.overlay) UVEditor.updatePastingOverlay();
 		})
+
 		return UVEditor;
 	}
 
@@ -76,11 +93,13 @@
 			})
 			MenuBar.addAction(about, "help");
 		}
+
 		aboutAction = new Action(`about_${id}`, {
 			name: `About ${name}...`,
 			icon,
 			click: () => showAbout()
 		})
+
 		about.children.push(aboutAction);
 	}
 
@@ -91,7 +110,7 @@
 			width: 780,
 			buttons: [],
 			lines: [`
-                <style>
+				<style>
 					dialog#about .dialog_title {
 						padding-left: 0;
 						display: flex;
@@ -118,9 +137,9 @@
 					dialog#about #content {
 						margin: 24px;
 					}
-                </style>
-                ${banner ? `<div id="banner">This window can be reopened at any time from <strong>Help > About Plugins > ${name}</strong></div>` : ""}
-                <div id="content">
+				</style>
+				${banner ? `<div id="banner">This window can be reopened at any time from <strong>Help > About Plugins > ${name}</strong></div>` : ""}
+				<div id="content">
 					<h1 style="margin-top:-10px">${name}</h1>
 					<p>Adds a button that centers the image viewport in an Image format.</p>
 					<h4>Worth noting:</h4>
@@ -139,12 +158,12 @@
 							<label>Discord Server</label>
 						</a>
 					</div>
-                </div>
+				</div>
             `]
 		}).show()
 		$("dialog#about .dialog_title").html(`
-    <i class="icon material-icons">${icon}</i>
-    ${name}
-    `)
+    		<i class="icon material-icons">${icon}</i>
+    		${name}
+    	`)
 	}
 })()
