114efeee136f0155522e3ece7b3eca75154d1460
ThatGravyBoat <thatgravyboat@gmail.com>
2023-01-15 10:40:27 -0330

    Move functions into self-invoking function

--- plugins/highlight_generator.js
+++ plugins/highlight_generator.js
@@ -31,19 +31,28 @@
         }
     });
 
-})();
+    function generateFile(){
+        let data = [];
+        for (let cube of Cube.all) {
+            const formattedLines = cube.getGlobalVertexPositions().map(point => point.map(value => value / 16.0));
+            const convertedVertices = [
+                [formattedLines[2], formattedLines[3], formattedLines[6], formattedLines[7]],
+                [formattedLines[0], formattedLines[1], formattedLines[4], formattedLines[5]]
+            ]
 
-function generateFile(){
-    let data = [];
-    for (let cube of Cube.all) {
-        const formattedLines = cube.getGlobalVertexPositions().map(point => point.map(value => value / 16.0));
-        const convertedVertices = [
-            [formattedLines[2], formattedLines[3], formattedLines[6], formattedLines[7]],
-            [formattedLines[0], formattedLines[1], formattedLines[4], formattedLines[5]]
-        ]
+            for (let convertedVertex of convertedVertices) {
+                for (let edge of createHorizontalEdges(convertedVertex)) {
+                    let topFace = []
+                    topFace.push(...edge[0])
+                    topFace.push(...edge[1])
+                    if (!data.includes(topFace)) {
+                        data.push(topFace)
+                    }
+                }
+                data.push(null)
+            }
 
-        for (let convertedVertex of convertedVertices) {
-            for (let edge of createHorizontalEdges(convertedVertex)) {
+            for (let edge of createVerticalEdges(convertedVertices[0], convertedVertices[1])) {
                 let topFace = []
                 topFace.push(...edge[0])
                 topFace.push(...edge[1])
@@ -53,34 +62,25 @@ function generateFile(){
             }
             data.push(null)
         }
-
-        for (let edge of createVerticalEdges(convertedVertices[0], convertedVertices[1])) {
-            let topFace = []
-            topFace.push(...edge[0])
-            topFace.push(...edge[1])
-            if (!data.includes(topFace)) {
-                data.push(topFace)
+        data.pop()
+        // The json is formatted below like this so its easier to read as each "section" will be seperated by a newline.
+        let output = `{\n    "lines": [\n`
+        for (let datum of data) {
+            if (datum == null) {
+                output += `\n`
+            } else {
+                output += `        [${datum[0]}, ${datum[1]}, ${datum[2]}, ${datum[3]}, ${datum[4]}, ${datum[5]}],\n`
             }
         }
-        data.push(null)
+        return output.substr(0, output.length - 2) + `\n    ]\n}`;
     }
-    data.pop()
-    // The json is formatted below like this so its easier to read as each "section" will be seperated by a newline.
-    let output = `{\n    "lines": [\n`
-    for (let datum of data) {
-        if (datum == null) {
-            output += `\n`
-        } else {
-            output += `        [${datum[0]}, ${datum[1]}, ${datum[2]}, ${datum[3]}, ${datum[4]}, ${datum[5]}],\n`
-        }
+
+    function createHorizontalEdges(vertices) {
+        return [[vertices[2], vertices[3]], [vertices[2], vertices[1]], [vertices[1], vertices[0]], [vertices[3], vertices[0]]]
     }
-    return output.substr(0, output.length - 2) + `\n    ]\n}`;
-}
 
-function createHorizontalEdges(vertices) {
-    return [[vertices[2], vertices[3]], [vertices[2], vertices[1]], [vertices[1], vertices[0]], [vertices[3], vertices[0]]]
-}
+    function createVerticalEdges(top, bottom) {
+        return [[bottom[3], top[3]], [bottom[2], top[2]], [bottom[1], top[1]], [bottom[0], top[0]]]
+    }
 
-function createVerticalEdges(top, bottom) {
-    return [[bottom[3], top[3]], [bottom[2], top[2]], [bottom[1], top[1]], [bottom[0], top[0]]]
-}
+})();
