b3c14a5afb86bef27be8fefbac0fbc38a440501b
McHorse <mchorselessrider@gmail.com>
2023-04-03 13:51:04 +0100

    BBS model exporter: Fix keyframe exporter mishandling Effect keyframes

--- plugins/bbs_exporter.js
+++ plugins/bbs_exporter.js
@@ -226,6 +226,11 @@
 
     function createAnimationKeyframes(g)
     {
+        if (!g)
+        {
+            return [];
+        }
+
         var keyframes = [];
 
         for (let i = 0; i < g.length; i++)
@@ -351,6 +356,11 @@
         remember: false,
         compile(options) {
             return autoStringify(compile());
+        },
+        fileName() {
+            var name = Project.name || "model";
+
+            return name.endsWith("bbs") ? name : name + ".bbs";
         }
     });
 
@@ -413,13 +423,13 @@
         author: "McHorse",
         description: "Adds a model exporter which allows to export models in BBS model format",
         icon: "fa-file-export",
-        version: "1.1.1",
+        version: "1.1.2",
         variant: "both",
         onload() {
             button = new Action("bbs_exporter", {
                 name: "Export BBS model",
                 category: "file",
-                description: "Export model as a BBS (.json) model",
+                description: "Export model as a BBS (.bbs.json) model",
                 icon: "fa-file-export",
                 click() {
                     exportDialog.show();
