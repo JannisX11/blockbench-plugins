a907578600e1a900736ceee613405ce0becb069d
Josiah Glosson <soujournme@gmail.com>
2023-04-10 17:11:29 -0500

    Move functions inside top-level function

--- plugins/animation_to_jsonea.js
+++ plugins/animation_to_jsonea.js
@@ -36,48 +36,48 @@
             menuButton.delete();
         }
     });
+    
+    function generateJson(animation) {
+        const result = {
+            length: animation.length,
+            animations: []
+        };
+        if (animation.loop == "loop") {
+            result.loop = true;
+        }
+        for (const id in animation.animators) {
+            const boneAnimator = animation.animators[id];
+            if (!(boneAnimator instanceof BoneAnimator)) continue;
+            if (boneAnimator.position.length) {
+                result.animations.push(generateKeyframes(boneAnimator.name, "position", boneAnimator.position));
+            }
+            if (boneAnimator.rotation.length) {
+                result.animations.push(generateKeyframes(boneAnimator.name, "rotation", boneAnimator.rotation));
+            }
+            if (boneAnimator.scale.length) {
+                result.animations.push(generateKeyframes(boneAnimator.name, "scale", boneAnimator.scale));
+            }
+        }
+        return result;
+    }
+
+    function generateKeyframes(bone, target, keyframes) {
+        const animData = {
+            bone,
+            target,
+            keyframes: []
+        };
+        for (const keyframe of [...keyframes].sort((a, b) => a.time - b.time)) {
+            animData.keyframes.push({
+                timestamp: keyframe.time,
+                target: [
+                    keyframe.get("x"),
+                    keyframe.get("y"),
+                    keyframe.get("z"),
+                ],
+                interpolation: keyframe.interpolation
+            });
+        }
+        return animData;
+    }
 })();
-
-function generateJson(animation) {
-    const result = {
-        length: animation.length,
-        animations: []
-    };
-    if (animation.loop == "loop") {
-        result.loop = true;
-    }
-    for (const id in animation.animators) {
-        const boneAnimator = animation.animators[id];
-        if (!(boneAnimator instanceof BoneAnimator)) continue;
-        if (boneAnimator.position.length) {
-            result.animations.push(generateKeyframes(boneAnimator.name, "position", boneAnimator.position));
-        }
-        if (boneAnimator.rotation.length) {
-            result.animations.push(generateKeyframes(boneAnimator.name, "rotation", boneAnimator.rotation));
-        }
-        if (boneAnimator.scale.length) {
-            result.animations.push(generateKeyframes(boneAnimator.name, "scale", boneAnimator.scale));
-        }
-    }
-    return result;
-}
-
-function generateKeyframes(bone, target, keyframes) {
-    const animData = {
-        bone,
-        target,
-        keyframes: []
-    };
-    for (const keyframe of [...keyframes].sort((a, b) => a.time - b.time)) {
-        animData.keyframes.push({
-            timestamp: keyframe.time,
-            target: [
-                keyframe.get("x"),
-                keyframe.get("y"),
-                keyframe.get("z"),
-            ],
-            interpolation: keyframe.interpolation
-        });
-    }
-    return animData;
-}
