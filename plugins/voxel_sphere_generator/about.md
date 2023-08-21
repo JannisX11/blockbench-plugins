5741ed883586598b1f9cb833f2b7fb86c1699f88
Doonv <58695417+doonv@users.noreply.github.com>
2023-08-21 22:41:38 +0300

    Rename Sphere Generator and add to plugins.json

--- plugins/voxel_sphere_generator/about.md
+++ plugins/voxel_sphere_generator/about.md
@@ -2,4 +2,4 @@ This plugin allows you to generate voxel spheres easily.
 
 You can find the sphere generation menu in **Tools > Generate Sphere**.
 
-Note that the spheres arent really that optimized and they use a lot of objects.
+Note that the spheres arent really that optimized and they can use a lot of objects.
--- plugins/voxel_sphere_generator/voxel_sphere_generator.js
+++ plugins/voxel_sphere_generator/voxel_sphere_generator.js
@@ -1,41 +1,36 @@
 /// <reference path="../../types/index.d.ts" />
 
 (function () {
-	BBPlugin.register("sphere_generator", {
-		title: "Sphere Generator",
+	BBPlugin.register("voxel_sphere_generator", {
+		title: "Voxel Sphere Generator",
 		author: "Doonv",
 		icon: "logo.svg",
-		description: "Generates spheres.",
+		description: "Generates voxel spheres.",
 		version: "0.1.0",
 		min_version: "4.8.0",
-		tags: ["Shapes", "Generation"],
+		tags: ["Shapes", "Generation", "Voxel"],
 		variant: "both",
 		onload() {
-			ShapeGeneratorAction = new Action("generate_spheres", {
-				name: "Generate Sphere",
-				description: "Generates a sphere.",
+			SphereGeneratorAction = new Action("generate_spheres", {
+				name: "Generate Voxel Sphere",
+				description: "Generates a voxel sphere.",
 				icon: "circle",
 				click: function () {
-
-
 					sphereWindow();
 				},
-
-			})
-			MenuBar.addAction(ShapeGeneratorAction, "filter");
+			});
+			MenuBar.addAction(SphereGeneratorAction, "filter");
 		},
 		onunload() {
-			ShapeGeneratorAction.delete();
-		}
+			SphereGeneratorAction.delete();
+		},
 	});
-
 
 	//sphere properties window
 	function sphereWindow() {
-
 		var sphere_window = new Dialog({
-			title: 'Sphere Options',
-			id: 'sphere_options',
+			title: "Sphere Options",
+			id: "sphere_options",
 
 			form: {
 				radius: { label: "Radius", type: "number", value: 5 },
@@ -51,14 +46,14 @@
 						lines_z: "Lines (Z Axis)",
 						lines_zy: "Lines (ZY Axis) (Experimental)",
 						none: "None (Very Slow!)",
-					}
+					},
 				},
 			},
 			draggable: true,
 			onConfirm(result) {
 				generateSphere(result);
 				sphere_window.hide();
-			}
+			},
 		});
 		sphere_window.show();
 	}
@@ -73,7 +68,7 @@
 		console.log("Creation Pass");
 
 		// Creation Pass
-		var cubes = []
+		var cubes = [];
 		for (let x = -radius; x <= radius; x++) {
 			cubes.push([]);
 			for (let y = -radius; y <= radius; y++) {
@@ -92,7 +87,7 @@
 		console.log("Optimization Pass");
 
 		// Optimization Pass
-		var boxes = []
+		var boxes = [];
 		switch (result.optimization_type) {
 			default:
 			case "lines_zy":
@@ -103,20 +98,21 @@
 							// if (cubes[x][y][z] == cubes[x][y][z + 1] == true) {\
 							if (cubes[x][y][z] == true) {
 								// console.log(`${boxes[boxes.length - 1].to} vs ${x}, ${y}, ${z - 1}`);
-								if (boxes[boxes.length - 1] &&
+								if (
+									boxes[boxes.length - 1] &&
 									boxes[boxes.length - 1].to[0] == x &&
 									boxes[boxes.length - 1].to[1] == y &&
-									boxes[boxes.length - 1].to[2] == z - 1) {
+									boxes[boxes.length - 1].to[2] == z - 1
+								) {
 									// There was a box here? Great! Extend it.
-									boxes[boxes.length - 1].to = [x, y, z]
+									boxes[boxes.length - 1].to = [x, y, z];
 									// console.log(`optimization? ${x}, ${y}, ${z}`);
 								} else {
 									// No box? Create one
-									boxes.push({ from: [x, y, z], to: [x, y, z] })
+									boxes.push({ from: [x, y, z], to: [x, y, z] });
 								}
 							}
 						}
-
 					}
 				}
 				if (result.optimization_type == "lines_zy") {
@@ -124,21 +120,22 @@
 					var changes = 1;
 					for (let iter = 1; changes > 0; iter++) {
 						changes = 0;
-						var new_boxes = []
-						var ignore_list = []
+						var new_boxes = [];
+						var ignore_list = [];
 						for (let index = 0; index < boxes.length; index++) {
 							if (ignore_list.find((i) => i == index) != undefined) {
 								continue;
 							}
 							const idx_adr = index < boxes.length - 2 ? 2 : 1;
 
-							if (index < boxes.length - 1 &&
+							if (
+								index < boxes.length - 1 &&
 								boxes[index].to[0] == boxes[index + idx_adr].to[0] &&
 								boxes[index].to[1] == boxes[index + idx_adr].to[1] - iter &&
-								boxes[index].to[2] == boxes[index + idx_adr].to[2]) {
-
-								boxes[index].to[1] = boxes[index + idx_adr].to[1]
-								ignore_list.push(index + idx_adr)
+								boxes[index].to[2] == boxes[index + idx_adr].to[2]
+							) {
+								boxes[index].to[1] = boxes[index + idx_adr].to[1];
+								ignore_list.push(index + idx_adr);
 								changes++;
 							}
 							new_boxes.push(boxes[index]);
@@ -153,7 +150,7 @@
 					for (let y = 0; y <= radius * 2; y++) {
 						for (let z = 0; z <= radius * 2; z++) {
 							if (cubes[x][y][z] == true) {
-								boxes.push({ from: [x, y, z], to: [x, y, z] })
+								boxes.push({ from: [x, y, z], to: [x, y, z] });
 							}
 						}
 					}
@@ -162,10 +159,11 @@
 		}
 
 		console.log("Addition Pass");
+		var cubes = [];
 		// Addition Pass
 		function additionPass() {
 			var sphere_group = new Group().init();
-			sphere_group.name = `Sphere Radius ${radius}`
+			sphere_group.name = `Sphere Radius ${radius}`;
 
 			for (let index = 0; index < boxes.length; index++) {
 				const box = boxes[index];
@@ -177,13 +175,18 @@
 				const toY = box.to[1];
 				const toZ = box.to[2];
 
-				new Cube({
-					name: `Cube ${fromX} ${fromY} ${fromZ} (${index})`,
-					from: [fromX, fromY, fromZ],
-					to: [toX + 1, toY + 1, toZ + 1],
-					rotation: { origin: [0, 0, 0], axis: "X", angle: 0 }
-				}).addTo(sphere_group).init();
+				cubes.push(
+					new Cube({
+						name: `Cube ${fromX} ${fromY} ${fromZ} (${index})`,
+						from: [fromX, fromY, fromZ],
+						to: [toX + 1, toY + 1, toZ + 1],
+						rotation: { origin: [0, 0, 0], axis: "X", angle: 0 },
+					})
+						.addTo(sphere_group)
+						.init()
+				);
 			}
+			return cubes;
 		}
 		if (1000 < boxes.length) {
 			var confirm_dialog = new Dialog("confirm_large_objects_dialog", {
@@ -191,22 +194,25 @@
 				lines: [
 					`The sphere about to be generated contains <strong>${boxes.length}</strong> objects.</br>`,
 					"This amount of objectives can lag lower-end devices.</br>",
-					"Are you <strong>sure</strong> you want to generate the sphere?"
+					"Are you <strong>sure</strong> you want to generate the sphere?",
 				],
 				onConfirm: additionPass,
 				onCancel() {
-					confirm_dialog.hide()
-				}
-			})
-			confirm_dialog.show()
+					confirm_dialog.hide();
+				},
+			});
+			confirm_dialog.show();
 		} else {
-			additionPass()
+			Undo.initEdit({ elements: [] });
+			additionPass();
+			Undo.finishEdit(`Add Sphere with radius ${radius}`, {
+				elements: [new_cube, other_cube],
+			});
 		}
-
 	}
-})()
+})();
 
 onUninstall = function () {
 	//Removing entries
-	Blockbench.removeMenuEntry('Generate Sphere')
+	Blockbench.removeMenuEntry("Generate Sphere");
 };
