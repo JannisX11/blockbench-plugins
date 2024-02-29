3fc9f89c2b4f1a02adde558324635e2d177d08df
Malik12tree <82341209+Malik12tree@users.noreply.github.com>
2024-02-29 19:49:49 +0200

    Better English

--- src/mesh_tools/src/actions.js
+++ src/mesh_tools/src/actions.js
@@ -2,75 +2,72 @@ const MESH_CONDITION = {
   modes: ["edit"],
   features: ["meshes"],
 };
-const OBJECT_MODE_CONDITION = Object.assign(
-  {
-    method: () =>
-      Mesh.selected.length && BarItems["selection_mode"].value == "object",
-  },
-  MESH_CONDITION
-);
-const NON_OBJECT_MODE_CONDITION = Object.assign(
-  {
-    method: () =>
-      Mesh.selected.length && BarItems["selection_mode"].value != "object",
-  },
-  MESH_CONDITION
-);
-const FACE_MODE_CONDITION = Object.assign(
-  {
-    method: () =>
-      Mesh.selected.length && BarItems["selection_mode"].value == "face",
-  },
-  MESH_CONDITION
-);
-const VERTEX_MODE_CONDITION = Object.assign(
-  {
-    method: () =>
-      Mesh.selected.length && BarItems["selection_mode"].value == "vertex",
-  },
-  MESH_CONDITION
-);
+const OBJECT_MODE_CONDITION = {
+  modes: ["edit"],
+  features: ["meshes"],
+  method: () =>
+    Mesh.selected.length && BarItems["selection_mode"].value == "object",
+};
+const NON_OBJECT_MODE_CONDITION = {
+  modes: ["edit"],
+  features: ["meshes"],
+  method: () =>
+    Mesh.selected.length && BarItems["selection_mode"].value != "object",
+};
+const FACE_MODE_CONDITION = {
+  modes: ["edit"],
+  features: ["meshes"],
+  method: () =>
+    Mesh.selected.length && BarItems["selection_mode"].value == "face",
+};
+const VERTEX_MODE_CONDITION = {
+  modes: ["edit"],
+  features: ["meshes"],
+  method: () =>
+    Mesh.selected.length && BarItems["selection_mode"].value == "vertex",
+};
 
 const ACTIONS = /** @type {const}  */ ({
   laplacian_smooth: {
     name: "Laplacian Smooth",
     icon: "blur_on",
     description:
-      "Smoothes selected vertices by averaging the position of neighboring vertices",
+      "Smoothens selected vertices by averaging the position of neighboring vertices.",
     condition: NON_OBJECT_MODE_CONDITION,
   },
   to_sphere: {
     name: "To Sphere",
     icon: "change_circle",
-    description: "Casts selected vertices into a sphere based on an influence",
+    description:
+      "Casts selected vertices into a smooth, spherical shape with adjustable influence.",
     condition: NON_OBJECT_MODE_CONDITION,
   },
   poke: {
     name: "Poke Faces",
-    description: "Creates a fan out of a face",
+    description: "Generates a fan out of a face.",
     icon: "control_camera",
     condition: FACE_MODE_CONDITION,
   },
   tris_to_quad: {
-    name: "Triangles To Quad",
-    description: "Tries to dissolve adjacent triangles into a quad",
+    name: "Triangles To Quadrilaterals",
+    description: "Attempts to merge adjacent triangles into quadrilaterals.",
     icon: `fas.fa-external-link-square-alt`,
     condition: FACE_MODE_CONDITION,
   },
   triangulate: {
     name: "Triangulate Faces",
-    description: "Cuttes a face into triangles",
+    description: "Splits selected faces into triangles.",
     icon: "pie_chart_outline",
     condition: FACE_MODE_CONDITION,
   },
   uv_project_view: {
     name: "Project From View",
-    description: "Projects the selected faces to the UV map from the camera",
+    description: "Projects the selected faces to the UV map from the camera.",
     icon: "view_in_ar",
   },
   uv_turnaround_projection: {
     name: "Cubic Projection",
-    description: "Unwraps the UV map from the 6 sides of a cube",
+    description: "Unwraps the UV map from the 6 sides of a cube.",
     icon: "open_with",
   },
   uv_mapping: {
@@ -82,13 +79,15 @@ const ACTIONS = /** @type {const}  */ ({
   expand_selection: {
     name: "Expand Selection",
     icon: "unfold_more_double",
-    keybind: new Keybind({ key: "l", ctrl: true }),
+    description: "Expands the selection with neighboring vertices.",
+    keybind: { key: "l", ctrl: true },
     condition: VERTEX_MODE_CONDITION,
   },
   shrink_selection: {
     name: "Shrink Selection",
     icon: "unfold_less_double",
-    keybind: new Keybind({ key: "k", ctrl: true }),
+    description: "Shrinks the selection with neighboring vertices.",
+    keybind: { key: "k", ctrl: true },
     condition: VERTEX_MODE_CONDITION,
   },
   "": {
@@ -128,12 +127,12 @@ const ACTIONS = /** @type {const}  */ ({
     name: "Split Edges",
     icon: "vertical_split",
     description:
-      "Splits and duplicates edges within a mesh, breaking 'links' between faces around those split edges",
+      "Splits and duplicates edges within a mesh, breaking 'links' between faces around those split edges.",
     condition: OBJECT_MODE_CONDITION,
   },
   scatter: {
     name: "Scatter",
-    description: "scatters selected meshes on the active mesh",
+    description: "Scatters selected meshes on the active mesh.",
     icon: "scatter_plot",
     condition: OBJECT_MODE_CONDITION,
   },
@@ -141,7 +140,7 @@ const ACTIONS = /** @type {const}  */ ({
     name: "Array",
     icon: "fas.fa-layer-group",
     description:
-      "Creates an array of copies of the base object, with each copy being offset from the previous one",
+      "Generates an array of copies of the base object, with each copy being offset from the previous one.",
     condition: OBJECT_MODE_CONDITION,
   },
   /*  */
@@ -161,24 +160,26 @@ const ACTIONS = /** @type {const}  */ ({
   terrain_action: {
     name: "Terrain",
     description:
-      "Generates Terrains procedurally with fully customized settings",
+      "Generates terrains procedurally with fully customized settings.",
     icon: "terrain",
   },
   terrainse: {
     name: "Terrain Style Editor",
     description:
-      "Configure the values of the style `Custom` for the Terrain Generator",
+      "Configure the Custom color gradient style of the terrain generator.",
     icon: "draw",
   },
   textmesh: {
     name: "Text Mesh",
     icon: "format_size",
+    description:
+      "Converts text into a 3D object, ideal for creating signs or logos.",
   },
   xyzmathsurfacefunction: {
     name: "XYZ Math Surface",
     icon: "fas.fa-brain",
     description:
-      "Creates an xyz surface based on given inputs. Also contains already-made 23 presets!",
+      "Generates an xyz surface based on mathematical equations containing 23 pre-built presets!",
   },
   quickprimitives: {
     name: "Quick Primitives",
@@ -188,10 +189,13 @@ const ACTIONS = /** @type {const}  */ ({
   polyhedron: {
     name: "Polyhedron",
     icon: "offline_bolt",
+    description:
+      "Generate a polyhedron such as an Icosahedron, a Dodecahedron, an Octahedron and a Tetrahedron.",
   },
   torusknot: {
     name: "Torus Knot",
     icon: "offline_bolt",
+    description: "Generate a Torus Knot.",
   },
 });
 
@@ -210,6 +214,9 @@ export function action(id, click) {
   if (options.children) {
     // TODO qualify with parents
     options.children = options.children.map(qualifyName);
+  }
+  if (options.keybind) {
+    options.keybind = new Keybind(options.keybind);
   }
   return new Action(qualifyName(id), options);
 }
