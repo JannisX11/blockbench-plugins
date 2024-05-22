f968331a5f656735ae71b52f8280aeeb88747327
MrCrayfish <4958241+MrCrayfish@users.noreply.github.com>
2024-05-23 02:17:27 +0930

    Create group exporter plugin

---
+++ plugins/group_exporter/outliner_group_exporter.js
@@ -0,0 +1,101 @@
+(function() {
+    var separator;
+    var actionExportGroup;
+    var originalExportFunction;
+    var originalGroupOpenContext;
+    BBPlugin.register('outliner_group_exporter', {
+        title: 'Group Exporter',
+        author: 'MrCrayfish',
+        icon: 'icon.png',
+        description: 'A simple plugin to allow you to export a group from the outliner as a model. Only cubes inside the group will be exported.',
+        tags: ['Minecraft Java Edition'],
+        version: '0.0.1',
+        min_version: '4.10.1',
+        variant: 'both',
+        onload() {
+            // Add translations
+            window.Language.addTranslations('en', {
+                'action.export_outliner_group': "Export..."
+            });
+
+            // Setup action and separator
+            separator = new MenuSeparator('export');
+            actionExportGroup = new Action('export_outliner_group', {
+                icon: 'insert_drive_file',
+                category: 'export',
+                // Only allow on java_block format or custom formats that can support it (e.g. ones that use Cubes)
+                condition: () => Format.id === 'java_block' || Format.allowOutlinerGroupExporting
+            });
+
+            // Append the separator and action to group menu
+            Group.prototype.menu.addAction(separator);
+            Group.prototype.menu.addAction(actionExportGroup);
+
+            let restrictCubes = (restrict) => {
+                if(!restrict)
+                    return;
+
+                // Get the top parent group
+                let group = getCurrentGroup();
+
+                // If none selected, just return
+                if(!group) {
+                    return;
+                }
+
+                // Find children cubes in the group
+                let selectedCubes = [];
+                group.forEachChild(cube => {
+                    selectedCubes.push(cube);
+                }, Cube, false);
+
+                // Update export flag on all cubes
+                Cube.all.forEach(cube => {
+                    cube.export = selectedCubes.includes(cube);
+                });
+            }
+
+            // Resets the export flag on all cubes
+            let resetCubes = (restrict) => {
+                if(!restrict)
+                    return;
+                Cube.all.forEach(cube => {
+                    cube.export = true;
+                });
+            }
+
+            // Override export function
+            originalExportFunction = Codec.prototype.export;
+            Codec.prototype.export = async function () {
+                // Check if the menu is open. Is this okay for web/mobile app?
+                let restrict = actionExportGroup.menu_node.classList.contains("opened");
+                restrictCubes(restrict); 
+                originalExportFunction.call(this);
+                resetCubes(restrict);
+            };
+
+            // Patch the context menu to include export options. Have to do this lazily
+            let patchedMenu = false;
+            originalGroupOpenContext = Group.prototype.showContextMenu;
+            Group.prototype.showContextMenu = function(event) {
+                if(!patchedMenu) {
+                    MenuBar.menus.file.structure.forEach(menu => {
+                        if(menu.id === 'export') {
+                            actionExportGroup.children = menu.children.slice(0);
+                        }
+                    });
+                    patchedMenu = true;
+                }
+                originalGroupOpenContext.call(this, event);
+            }
+        },
+        onunload() {
+            // Clean up
+            Codec.prototype.export = originalExportFunction;
+            Group.prototype.showContextMenu = originalGroupOpenContext;
+            Group.prototype.menu.removeAction(separator);
+            Group.prototype.menu.removeAction(actionExportGroup);
+            actionExportGroup.delete();
+        }
+    });
+})()
