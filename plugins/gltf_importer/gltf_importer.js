80b62845f3dd07414374e001a7712be94be16af3
0x13F <22893194+mr0x13f@users.noreply.github.com>
2025-09-29 20:43:16 +0200

    Update gltf_importer.js

--- plugins/gltf_importer/gltf_importer.js
+++ plugins/gltf_importer/gltf_importer.js
@@ -2590,7 +2590,7 @@
     author: "0x13F",
     description: "Import .GLTF and .GLB models",
     icon: "icon.png",
-    creation_date: "//TODO:",
+    creation_date: "29-09-2025",
     version: "1.0.0",
     variant: "desktop",
     min_version: "4.12.6",
