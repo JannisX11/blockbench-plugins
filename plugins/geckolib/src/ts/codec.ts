dba87616ad74a2bbf2503713f460039ec41a7bf7
Tslat <7098525+Tslat@users.noreply.github.com>
2025-11-06 16:23:47 +1000

    Enable .png.mcmeta, enable UV rotation, clean up property ordering for format

--- plugins/geckolib/src/ts/codec.ts
+++ plugins/geckolib/src/ts/codec.ts
@@ -13,23 +13,25 @@ const codec = Codecs.bedrock;
 // This gets automatically applied by Blockbench, we don't need to do anything with it
 export const format = new ModelFormat(GECKOLIB_MODEL_ID, {
     id: GECKOLIB_MODEL_ID,
-    name: "GeckoLib Animated Model",
-    category: "minecraft",
-    description: "Animated Model for Java mods using GeckoLib",
     icon: "view_in_ar",
-    rotate_cubes: true,
+    name: "GeckoLib Animated Model",
+    description: "Animated Model for Java mods using GeckoLib",
+    category: "minecraft",
     box_uv: true,
     optional_box_uv: true,
     single_texture: true,
+    animated_textures: true,
     bone_rig: true,
     centered_grid: true,
-    animated_textures: true,
-    select_texture_for_particles: true,
-    animation_files: true,
+    rotate_cubes: true,
     locators: true,
-    codec: Codecs.project,
+    uv_rotation: true,
+    select_texture_for_particles: true,
+    texture_mcmeta: true,
+    animation_files: true,
     display_mode: false,
     animation_mode: true,
+    codec: Codecs.project,
 })
 
 // Override the new project panel to allow customisation
