8a6c2ce8b57fba469b2c315474ce608b806d5cb7
honzajan722 <honzajan722@gmail.com>
2026-07-02 19:16:27 +0200

    Add voxelizer plugin for mesh conversion
    This plugin converts Mesh geometry into standard Java Cubes while preserving the original position, size, and rotation.

---
+++ plugins/voxelizer/voxelizer-plugin.js
@@ -0,0 +1,15 @@
+(function() {
+    Plugin.register('voxelizer', {
+        title: 'Voxelizer',
+        author: 'PAT1519',
+        icon: 'icon.png',
+        description: 'Converts Mesh geometry into standard Java Cubes while preserving original position, size, and rotation.',
+        version: '1.0.0',
+        min_version: '4.8.0',
+        variant: 'both',
+        onload() {
+        },
+        onunload() {
+        }
+    });
+})();
