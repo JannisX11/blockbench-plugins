a96c6a02e082fe943762c512931c2ef11723e752
honzajan722 <honzajan722@gmail.com>
2026-07-04 13:20:56 +0200

    Add files via upload

---
+++ plugins/Voxelizer/voxelizer.js
@@ -0,0 +1,18 @@
+(function() {
+    Plugin.register('voxelizer', {
+        title: 'Voxelizer',
+        author: 'PAT1519',
+        icon: 'icon.png',
+        description: 'PRE-RELEASE STUB: Future utility tool to convert Mesh geometry into standard Java Cubes.',
+        version: '1.0.0',
+        min_version: '4.8.0',
+        variant: 'both',
+        has_changelog: true,
+        onload() {
+            // Functional code will be implemented in the next release.
+        },
+        onunload() {
+            // Clean up functions for actions.
+        }
+    });
+})();
