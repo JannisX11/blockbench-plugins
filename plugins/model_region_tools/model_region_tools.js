7fc6bc9872f8e9189c1b1d031126981849e42551
NoviSun19 <novisun85@gmail.com>
2026-09-09 12:29:39 +0900

    Add min_version to model_region_tools.js
    Added min_version property to model region tools.

--- plugins/model_region_tools/model_region_tools.js
+++ plugins/model_region_tools/model_region_tools.js
@@ -3,8 +3,9 @@ Plugin.register('model_region_tools', {
     author: 'HorrorGirl',
     description: 'Tools for selecting regions directly on the 3D model.',
     icon: 'select_all',
-    version: '1.0.0',
-    variant: 'both',
+version: '1.0.0',
+min_version: '4.8.0',
+variant: 'both',
 
     onload() {
         this.region_select = new Action('model_region_tools_region_select', {
